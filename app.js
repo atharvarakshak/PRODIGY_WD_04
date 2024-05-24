8c9c8f6553ad635d7cfe165d07ddaed2557fb619
Atharva Rakshak <115388098+atharvarakshak@users.noreply.github.com>
2024-05-24 11:50:43 +0530

    task 4

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
 let turnO = true; //playerX, playerO
-let count = 0; //To Track Draw
+let count = 0; //To  Draw
 
 const winPatterns = [
   [0, 1, 2],
